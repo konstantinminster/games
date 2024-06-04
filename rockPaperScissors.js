1e315eff18e4ef0924fae0efff40a4ffea783404
konstantinminster <konstantin@minster.biz>
2024-06-04 09:21:22 +0200

    a bit of reducing the else if's

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -4,33 +4,18 @@ const options = ["Rock", "Paper", "Scissors"]
 const userPick = args;
 const compPick = options[Math.floor(Math.random() * options.length)];
 
-// Möglich kombinationen
-// Rock / Paper = Paper wins
-// Rock / Scissors = Rock wins
-// Paper / Scissors = Scissors wins
-
 if (userPick == compPick) {
     console.log(`Beide haben ${args}! Es ist unentschieden`);
-} else if (userPick == "Rock" && compPick == "Paper" ) {
+} else if (userPick == "Rock" && compPick == "Paper" || userPick == "Paper" && compPick == "Scissors" || userPick == "Scissors" && compPick == "Rock"  ) {
     console.log(`Du hast ${userPick}! Der Computer hat ${compPick}! Der Computer gewinnt!`); // Stein verliegt gegen Papier
-} else if (userPick == "Paper" && compPick == "Rock" ) {
+} else if (userPick == "Paper" && compPick == "Rock" || userPick == "Scissors" && compPick == "Paper" || userPick == "Rock" && compPick == "Scissors") {
     console.log(`Du hast ${userPick}! Der Computer hat ${compPick}! Du gewinnst!`); // Papier schlägt Stein
-} else if (userPick == "Paper" && compPick == "Scissors" ) {
-    console.log(`Du hast ${userPick}! Der Computer hat ${compPick}! Der Computer gewinnt!`); // Papier verliert gegen Schere
-} else if (userPick == "Scissors" && compPick == "Paper" ) {
-    console.log(`Du hast ${userPick}! Der Computer hat ${compPick}! Du gewinnst!`); // Schäre schlägt papier
-} else if (userPick == "Scissors" && compPick == "Rock" ) {
-    console.log(`Du hast ${userPick}! Der Computer hat ${compPick}! Der Computer gewinnt!`); // Stein schlägt schere
-} else if (userPick == "Rock" && compPick == "Scissors" ) {
-    console.log(`Du hast ${userPick}! Der Computer hat ${compPick}! Du gewinnst!`); // Schere verliert gegen Stein
-} else {
+}  else {
     console.log("Something went wrong");
 }
-
 
 // check if user pick something from the array
 const found = options.find((element) => element == args );
 if (found == undefined) {
     console.log("Bitte wähle zwischen " + options.join(", "));
 }
-
